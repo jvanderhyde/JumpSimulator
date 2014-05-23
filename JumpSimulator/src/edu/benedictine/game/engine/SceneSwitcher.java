3dc63751e23b82b232a34e9a8b3afd100e031ecc
ignatiusraven <ignatiusraven@gmail.com>
2014-05-23 16:45:00 -0500

    Update SceneSwitcher.java

--- JumpSimulator/src/edu/benedictine/game/engine/SceneSwitcher.java
+++ JumpSimulator/src/edu/benedictine/game/engine/SceneSwitcher.java
@@ -1,3 +1,6 @@
+//Modified by Joseph Rioux, 23 May 2014
+//added comments
+
 package edu.benedictine.game.engine;
 
 import edu.benedictine.game.gui.Director;
@@ -18,14 +21,18 @@ public class SceneSwitcher
 	
 	public void mark(String index, int time, double nX, double nY)
 	{
+		//set the number of frames that will elapse before the scene is changed
 		timeToGo = time;
+		//set the next scene to load, as a string
 		nextScene = index;
+		//set the player's position in the new scene
 		nextX = nX;
 		nextY = nY;
 	}
 	
 	public void update()
 	{
+		//when the timer runs out, change the scene
 		if (timeToGo == 0)
 			mn.changeScene(nextScene, mn.currentScene.currentLevel, nextX, nextY);
 		timeToGo--;
