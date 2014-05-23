ee2a8adee7acb77a0bc37ae03cd718077390df80
ignatiusraven <ignatiusraven@gmail.com>
2014-05-23 16:54:35 -0500

    Update SceneObject.java

--- JumpSimulator/src/edu/benedictine/game/engine/SceneObject.java
+++ JumpSimulator/src/edu/benedictine/game/engine/SceneObject.java
@@ -1,14 +1,17 @@
 //created by Joseph Rioux, 4 March 2013
+//modified by Joseph Rioux, 23 May 2014
+	//added isAlive(), added comments
 
 package edu.benedictine.game.engine;
 
 import edu.benedictine.game.gui.Scene;
 
-//	any object updated by the scene
+//any object updated by the scene
 
 public class SceneObject 
 {
 	int execOrder, lifeTime = -1;
+	public boolean alive=true;
 	double xSpeed, ySpeed;
 	protected Scene scn;
 	
@@ -43,6 +46,10 @@ public class SceneObject
 	
 	public void die()
 	{
+		//set alive to false so that any object referencing this one through
+		//another pointer can still tell that it has been removed from the current scene
+		alive = false;
+		//remove from the current scene
 		scn.objs.remove(this, execOrder);
 	}
 	
@@ -61,4 +68,10 @@ public class SceneObject
 		return ySpeed;
 	}
 	
+	public boolean isAlive()
+	{
+		//this is used to for check whether an object that you are referencing through
+		//another pointer is still in the current scene.
+		return alive;
+	}
 }
